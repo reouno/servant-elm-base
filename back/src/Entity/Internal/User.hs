c17aac664e8066bae01e6e9540d876e3f1becc31
宇野礼於 <leouno12@gmail.com>
2019-12-11 08:13:48 +0900

    :recycle: modularize Entity

---
+++ back/src/Entity/Internal/User.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedLabels  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
+
+module Entity.Internal.User where
+
+import           Control.Lens
+import           Data.Extensible
+import           Data.Text
+import           Data.Time       ( UTCTime )
+
+type User
+   = Record '[ "name" >: Text, "email" >: Maybe Text, "createdAt" >: UTCTime]
+
+type UserId = Int
+
+type UserRecord = (UserId, User)
+
+user1 :: User
+user1 =
+  #name @= "Neo" <: #email @= Just "neo@matrix.mov" <: #createdAt @=
+  (read "1999-09-11 00:00:00" :: UTCTime) <:
+  emptyRecord
+
+{-
+ - user1 ^. #name
+ - --> "Neo"
+ -}
+user1Id :: UserId
+user1Id = 1
+
+user1Record :: UserRecord
+user1Record = (user1Id, user1)
+
+user2 :: User
+user2 =
+  #name @= "Morpheus" <: #email @= Nothing <: #createdAt @=
+  (read "1812-09-11 00:00:00" :: UTCTime) <:
+  emptyRecord
+
+user2Id :: UserId
+user2Id = 2
+
+user2Record :: UserRecord
+user2Record = (user2Id, user2)
