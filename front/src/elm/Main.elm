147dc5476673542970d6327e030aec2f1f26e71e
Leo UNO <leouno12@gmail.com>
2020-01-19 17:26:36 +0900

    :sparkles: Elm application scaffold

--- front/src/elm/Main.elm
+++ front/src/elm/Main.elm
@@ -1,20 +1,60 @@
 module Main exposing (..)
 
 import Browser
+import Browser.Navigation as Nav
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Url
 
-main = Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
 
-update msg model = case msg of
-    Increment -> model + 1
-    Decrement -> model - 1
 
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    }
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    ( Model key url, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Browser.Document Msg
 view model =
-    div []
-        [ button [onClick Decrement] [text "-"]
-        , div [] [text (String.fromInt model)]
-        , button [onClick Increment] [text "+"]
-        ]
+    { title = "Simple Post", body = [ text "Hello, world." ] }
