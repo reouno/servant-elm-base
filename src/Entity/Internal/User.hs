b77e5f788dcbbb790254e7d5732cf19c7acf5428
宇野礼於 <leouno12@gmail.com>
2020-01-30 08:17:58 +0900

    :art: reformat with brittany

--- src/Entity/Internal/User.hs
+++ src/Entity/Internal/User.hs
@@ -7,15 +7,16 @@ module Entity.Internal.User where
 import           Control.Lens
 import           Data.Extensible
 import           Data.Text
-import           Data.Time.Clock.POSIX ( POSIXTime )
-import           Elm.Derive            ( defaultOptions, deriveBoth )
+import           Data.Time.Clock.POSIX          ( POSIXTime )
+import           Elm.Derive                     ( defaultOptions
+                                                , deriveBoth
+                                                )
 
 type Email = Text
 
 type UserUniqueKey = Email
 
-type User
-   = Record '[ "name" >: Text, "email" >: Email, "createdAt" >: POSIXTime]
+type User = Record '["name" >: Text, "email" >: Email, "createdAt" >: POSIXTime]
 
 type UserId = Int
 
--- src/InterfaceAdapter/PersistentStore/Model/User/Adapter.hs
+++ src/InterfaceAdapter/PersistentStore/Model/User/Adapter.hs
@@ -7,15 +7,25 @@ module InterfaceAdapter.PersistentStore.Model.User.Adapter where
 import           Control.Lens
 import           Data.Extensible
 import           Data.Text
-import           Data.Time.Clock.POSIX                  ( posixSecondsToUTCTime,
-                                                          utcTimeToPOSIXSeconds )
-import           Database.Persist.Sql                   ( entityKey, entityVal )
-import           Database.Persist.Types                 ( Entity )
+import           Data.Time.Clock.POSIX          ( posixSecondsToUTCTime
+                                                , utcTimeToPOSIXSeconds
+                                                )
+import           Database.Persist.Sql           ( entityKey
+                                                , entityVal
+                                                )
+import           Database.Persist.Types         ( Entity )
 
-import           Entity.Entity                          ( User (..), UserId, UserRecord,
-                                                          UserUniqueKey )
-import qualified InterfaceAdapter.PersistentStore.Model as M
-import           PersistentUtil                         ( entity2Tuple, int2SqlKey, sqlKey2Int )
+import           Entity.Entity                  ( User(..)
+                                                , UserId
+                                                , UserRecord
+                                                , UserUniqueKey
+                                                )
+import qualified InterfaceAdapter.PersistentStore.Model
+                                               as M
+import           PersistentUtil                 ( entity2Tuple
+                                                , int2SqlKey
+                                                , sqlKey2Int
+                                                )
 
 fromEntityUserId :: UserId -> M.UserId
 fromEntityUserId = int2SqlKey
@@ -24,11 +34,9 @@ fromEntityUserUniqueKey :: UserUniqueKey -> M.Unique M.User
 fromEntityUserUniqueKey = M.UniqueEmail
 
 fromEntityUser :: User -> M.User
-fromEntityUser user =
-  M.User
-    (user ^. #name)
-    (user ^. #email)
-    (posixSecondsToUTCTime $ user ^. #createdAt)
+fromEntityUser user = M.User (user ^. #name)
+                             (user ^. #email)
+                             (posixSecondsToUTCTime $ user ^. #createdAt)
 
 toEntityUserId :: M.UserId -> UserId
 toEntityUserId = sqlKey2Int
@@ -37,10 +45,14 @@ toEntityUserUniqueKey :: M.Unique M.User -> UserUniqueKey
 toEntityUserUniqueKey (M.UniqueEmail email) = email
 
 toEntityUser :: M.User -> User
-toEntityUser (M.User name email createdAt) =
-  #name @= name <: #email @= email <: #createdAt @=
-  utcTimeToPOSIXSeconds createdAt <:
-  emptyRecord
+toEntityUser (M.User name email _) =
+  #name
+    @= name
+    <: #email
+    @= email
+    <: #createdAt
+    @= utcTimeToPOSIXSeconds createdAt
+    <: emptyRecord
 
 toEntityUserRecord :: Entity M.User -> UserRecord
 toEntityUserRecord user =
