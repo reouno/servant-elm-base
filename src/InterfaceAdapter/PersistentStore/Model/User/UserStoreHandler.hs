95ee07cb48ef8702519e873ec4144517e1a583ff
宇野礼於 <leouno12@gmail.com>
2020-01-30 08:20:58 +0900

    :art: reformat with brittany

--- src/InterfaceAdapter/PersistentStore/Model/User/UserStoreHandler.hs
+++ src/InterfaceAdapter/PersistentStore/Model/User/UserStoreHandler.hs
@@ -6,30 +6,38 @@
 -- {-# LANGUAGE MultiParamTypeClasses #-}
 module InterfaceAdapter.PersistentStore.Model.User.UserStoreHandler where
 
-import           Conduit                                               ( MonadUnliftIO )
-import           Control.Monad.IO.Class                                ( MonadIO )
-import           Data.Pool                                             ( Pool )
+import           Conduit                        ( MonadUnliftIO )
+import           Control.Monad.IO.Class         ( MonadIO )
+import           Data.Pool                      ( Pool )
 import           Database.Persist
 import           Database.Persist.Sql
-import           PersistentUtil                                        ( delete', entity2Tuple,
-                                                                         get', getBy', insert',
-                                                                         replace', selectList' )
+import           PersistentUtil                 ( delete'
+                                                , entity2Tuple
+                                                , get'
+                                                , getBy'
+                                                , insert'
+                                                , replace'
+                                                , selectList'
+                                                )
 
 -- import           InterfaceAdapter.PersistentStore.Model
 import           Entity.Entity
-import           InterfaceAdapter.PersistentStore.Infra.Postgres.Types ( PgPool )
-import           InterfaceAdapter.PersistentStore.Model.User.Adapter   ( fromEntityUser,
-                                                                         fromEntityUserId,
-                                                                         fromEntityUserUniqueKey,
-                                                                         toEntityUser,
-                                                                         toEntityUserId,
-                                                                         toEntityUserRecord )
-import           Usecase.Interface.User.UserStore                      ( UserStore (..) )
+import           InterfaceAdapter.PersistentStore.Infra.Postgres.Types
+                                                ( PgPool )
+import           InterfaceAdapter.PersistentStore.Model.User.Adapter
+                                                ( fromEntityUser
+                                                , fromEntityUserId
+                                                , fromEntityUserUniqueKey
+                                                , toEntityUser
+                                                , toEntityUserId
+                                                , toEntityUserRecord
+                                                )
+import           Usecase.Interface.User.UserStore
+                                                ( UserStore(..) )
 
 instance UserStore PgPool where
   getUsers pool = map (adapter . entity2Tuple) <$> selectList' pool
-    where
-      adapter (a, b) = (a, toEntityUser b)
+    where adapter (a, b) = (a, toEntityUser b)
   getUser pool key = fmap toEntityUser <$> get' (fromEntityUserId key) pool
   getUserBy pool uniqueKey =
     fmap toEntityUserRecord <$> getBy' (fromEntityUserUniqueKey uniqueKey) pool
